3622bb7d15bb9dded81e2c4e760120875d59cc11
simsint <sod.simsint@gmail.com>
2021-08-11 03:36:18 +0530

    updates
    updates S.A.H local guide url, added a link to temporarly allow CORS for websites functionality for demo purpose.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,58 +10,69 @@ console.table(fields);
 
 $("#fields").text(JSON.stringify(fields, undefined, 2));
 
-$('#submit').on('click',function(){
-    contents = getResponseText($('#new-link').val());
-    fillFields(contents);
-    $("#new-fields").text(JSON.stringify(fields, undefined, 2));
+$("#submit").on("click", function () {
+  contents = getResponseText($("#new-link").val());
+  fillFields(contents);
+  $("#new-fields").text(JSON.stringify(fields, undefined, 2));
 });
 
+function fillFields(contents) {
+  name = simple_match('<meta content="Contributions by ([^"]*)" itemprop="name">', contents);
+  fields.name = name;
+  fields.profile_image = simple_match(
+    '\\[\\\\"' + name + '\\\\",\\[null,4,null,null,null,null,\\[\\\\"([^\\\\]*)\\\\"]',
+    contents
+  );
+  fields.contributions = simple_match(
+    ',\\\\\\"(([\\d]{1,3},)?([\\d]{3},)*([\\d]{3})|([\\d]{1-3})) contributions\\\\\\"',
+    contents
+  );
+  fields.photo_view = photo_view_match('(\\d+),\\\\"(C[\\w-]{34,40})\\\\",(\\d+)', contents);
 
-function fillFields(contents){
-    name = simple_match('<meta content="Contributions by ([^"]*)" itemprop="name">',contents);
-    fields.name = name;
-    fields.profile_image = simple_match('\\[\\\\"' + name + '\\\\",\\[null,4,null,null,null,null,\\[\\\\"([^\\\\]*)\\\\"]', contents);
-    fields.contributions = simple_match(',\\\\\\"(([\\d]{1,3},)?([\\d]{3},)*([\\d]{3})|([\\d]{1-3})) contributions\\\\\\"',contents);
-    fields.photo_view = photo_view_match('(\\d+),\\\\"(C[\\w-]{34,40})\\\\",(\\d+)', contents);
-    
-    fields.points = simple_match('(([\\d]{1,3},)?([\\d]{3},)*([\\d]{3})|([\\d]{1-3})) Points" itemprop="description"', contents);
-    if (fields.points !== null) {
-      fields.level = simple_match('<meta content="Level (\\d+) Local Guide', contents);
-    } else {
-        fields.points = simple_match('(([\\d]{1,3},)?([\\d]{3},)*([\\d]{3})|([\\d]{1-3})) Contributions" itemprop="description"', contents);
-    }
-    
-    var contributions_type = [
-      "ratings",
-      "reviews",
-      "photos",
-      "videos",
-      "answers",
-      "edits",
-      "places added",
-      "road added",
-      "roads added",
-      "facts checked",
-      "Q\\\\u0026A",
-      "Q\\\\u0026As",
-      "published lists",
-    ];
-    
-    contributions_type.forEach(function (item) {
-      // var value = contribution_match("\\\\\"(\\d+) " + item + "\\\\\"]", contents);
-      var value = contribution_match(
-        '\\\\"(https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*))\\\\",((\\d+)),\\\\"[\\d,]+ ' +
-          item +
-          '\\\\"]',
-        contents
-      );
-      if (value !== null) {
-        //console.log(Object.entries(value).length);
-        if (Object.entries(value).length > 0) {
-          fields[item] = value;
-        }
+  fields.points = simple_match(
+    '(([\\d]{1,3},)?([\\d]{3},)*([\\d]{3})|([\\d]{1-3})) Points" itemprop="description"',
+    contents
+  );
+  if (fields.points !== null) {
+    fields.level = simple_match('<meta content="Level (\\d+) Local Guide', contents);
+  } else {
+    fields.points = simple_match(
+      '(([\\d]{1,3},)?([\\d]{3},)*([\\d]{3})|([\\d]{1-3})) Contributions" itemprop="description"',
+      contents
+    );
+  }
+
+  var contributions_type = [
+    "ratings",
+    "reviews",
+    "photos",
+    "videos",
+    "answers",
+    "edits",
+    "places added",
+    "road added",
+    "roads added",
+    "facts checked",
+    "Q\\\\u0026A",
+    "Q\\\\u0026As",
+    "published lists",
+  ];
+
+  contributions_type.forEach(function (item) {
+    // var value = contribution_match("\\\\\"(\\d+) " + item + "\\\\\"]", contents);
+    var value = contribution_match(
+      '\\\\"(https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*))\\\\",((\\d+)),\\\\"[\\d,]+ ' +
+        item +
+        '\\\\"]',
+      contents
+    );
+    if (value !== null) {
+      //console.log(Object.entries(value).length);
+      if (Object.entries(value).length > 0) {
+        fields[item] = value;
       }
-    });
+    }
+  });
 }
 
 function contribution_match(pattern, contents) {
@@ -81,6 +92,7 @@ function contribution_match(pattern, contents) {
 
 function getResponseText(link) {
   var cors = "https://cors-anywhere.herokuapp.com/";
+  // var cors = "";
   // return $.ajax({
   //     type: "GET",
   //     url: cors + link,
